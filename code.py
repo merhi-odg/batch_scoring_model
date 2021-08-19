75d1319b5bf7dfce4dee6ec13d84f97bf439ccf5
Sami Merhi <51339977+merhi-odg@users.noreply.github.com>
2021-08-19 12:47:05 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,9 @@ import logging
 import pandas
 
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(
+    level="INFO"
+)
 
 # modelop.init
 def begin():
@@ -16,10 +18,12 @@ def action(data):
     
     # data provided as an array of dictionaries, such as 
     # [{"a":1, "b":2}, {"a":3, "b":4}]
+    
     data = pandas.DataFrame(data)
     
     logger.info("BEFORE SUM: data is a dataframe of shape %s", str(data.shape))
     
+    # Add a sum column for all rows at-once
     data["rown_sum"]=data["a"]+data["b"]
     
     logger.info("AFTER SUM: data is a dataframe of shape %s", str(data.shape))
