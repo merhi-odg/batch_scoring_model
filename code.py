c370f41b8fec3eb038a3682aea3ef72d71fd8502
merhi-odg <sami.merhi@opendatagroup.com>
2022-08-25 09:53:59 -0400

    add csv input

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ logging.basicConfig(level="INFO")
 
 # modelop.init
 def begin():
-    
+
     # Unpickle any binaries if you have any in the scope of the init function
     # This is also a good place to declare any global variables
     pass
@@ -19,12 +19,12 @@ def begin():
 
 # modelop.score
 def action(data):
-        
+
     logger.info("BEFORE SUM: data is a dataframe of shape %s", str(data.shape))
-    
+
     # Add a sum column for all rows at-once
     data["rown_sum"] = data["a"] + data["b"]
-    
+
     logger.info("AFTER SUM: data is a dataframe of shape %s", str(data.shape))
-    
+
     yield data
